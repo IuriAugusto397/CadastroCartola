c3b1809cfb0962dea7dcfe5638628501cc9930cc
Iuri Augusto <yuriaugustosp@hotamil.com>
2019-09-29 13:57:01 -0300

    Adicionando background nas telas e alterando icone do aplicativo

--- app/src/main/java/com/example/cadastrocartola/MainActivity.java
+++ app/src/main/java/com/example/cadastrocartola/MainActivity.java
@@ -11,18 +11,30 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.mViewHolder.cadastrar_time = findViewById(R.id.cadastrar_time);
 
+        this.mViewHolder.cadastrar_time.setOnClickListener(this);
 
     }
 
+    private static class ViewHolder{
+        Button cadastrar_time;
+    }
 
+    @Override
+    public void onClick(View view) {
+    if(view.getId() == R.id.cadastrar_time ){
 
-
+    }
+    }
 }
