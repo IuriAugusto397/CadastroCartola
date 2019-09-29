0e02d0d687e1dee90ec4d9293659287ce50f8fa2
Iuri Augusto <yuriaugustosp@hotamil.com>
2019-09-29 12:17:35 -0300

    Adicionando telas main e cadastro_time

---
+++ app/src/main/java/com/example/cadastrocartola/telaDeCadastroTime.java
@@ -0,0 +1,14 @@
+package com.example.cadastrocartola;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class telaDeCadastroTime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_de_cadastro_time);
+    }
+}
